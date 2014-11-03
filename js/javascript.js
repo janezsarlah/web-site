a0fac34f5804361637097100f43158fef013eb9e
JanezSarlah <janez.sarlah@gmail.com>
2014-11-03 18:54:09 +0100

    Add Flexslider

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: 		"slide",
+    pauseOnHove: 	false,
+    controlNav: 	false,
+    smoothHeight: 	false,
+    directionNav: 	false
+  });
+});
