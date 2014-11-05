fe2f96b3bed393d1632c2489ad30298f95e35e59
JanezSarlah <janez.sarlah@gmail.com>
2014-11-05 13:08:38 +0100

    Add contatc, footer.

--- js/javascript.js
+++ js/javascript.js
@@ -36,4 +36,21 @@ $container.isotope({
   itemSelector : '.iso-item'
 });
 
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Change color of Contact Us inputs
+$(".floating-label-form-group").on("click", function() {
+    $(".floating-label-form-group").css("background", "whte");
+    $(".floating-label-form-group").css("opacity", "1"); 
+    $(".floating-label-form-group").css("border", "1px solid white"); 
+}); 
 
